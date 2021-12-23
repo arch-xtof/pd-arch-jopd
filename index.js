543b11378d79d229c483bd04591455c95d14f194
Archil Kristinashvili <achiko2001@gmail.com>
2021-12-23 17:57:38 +0200

    added redis

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const express = require("express");
 const axios = require("axios");
+const redis = require("redis");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
 const apiUrl = "http://play.grafana.org";
 const app = express();
+const client = redis.createClient({
+  host: '172.28.1.1',
+  port: 6379
+});
+
+client.on("error", () => console.log("error"));
+
+console.log(client);
+
+//(async () => {
+//  client.on('error', (err) => console.log('Redis Client Error', err));
+//  await client.connect();
+//})();
 
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get("/api/dashboards/:uid", async (request, response) => {
+app.get("/old/dashboards/:uid", async (request, response) => {
   let uid = request.params.uid;
   const responseObject = {};
 
   if (uid?.length <= 40) {
     await axios
-      .get(apiUrl + "/api/dashboards/uid/" + uid)
+      .get(`${apiUrl}/api/dashboards/uid/${uid}`)
       .then((res) => {
         transformDashboardJson(res.data, responseObject);
       })
@@ -30,8 +44,43 @@ app.get("/api/dashboards/:uid", async (request, response) => {
   response.json(responseObject);
 });
 
-function transformDashboardJson(data, responseObject) {
+app.get("/api/dashboards/:uid", async (request, response) => {
+  try {
+    let uid = request.params.uid;
+    if (uid?.length >= 40) {
+      return response.status(400).send({
+        message: `uid can't be longer than 40 characters`,
+      });
+    }
 
+    client.get(uid, async (err, dashboardInfo) => {
+      console.log("---yle---");
+      if (dashboardInfo) {
+        return response.status(200).send(JSON.parse(dashboardInfo));
+      } else {
+        const dashboardInfo = {};
+        await axios
+          .get(`${apiUrl}/api/dashboards/uid/${uid}`)
+          .then((axiosResponse) => {
+            transformDashboardJson(axiosResponse.data, dashboardInfo);
+          })
+          .catch((error) => {
+            return response.status(404).send({
+              message: error,
+            });
+          });
+
+        client.setex(uid, 1440, JSON.stringify(dashboardInfo));
+
+        return response.status(200).send(JSON.parse(dashboardInfo));
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+function transformDashboardJson(data, responseObject) {
   responseObject.uid = data.dashboard.uid;
   responseObject.title = data.dashboard.title;
   responseObject.url = apiUrl + data.meta.url;
